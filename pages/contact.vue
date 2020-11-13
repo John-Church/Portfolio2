debef4e3143cf82ce533c20894cc06db189e3740
John <churchjohnr@gmail.com>
2020-11-13 15:40:56 -0800

    fixed favicon color, margins on contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -8,8 +8,8 @@ name="contact"
   netlify
   netlify-honeypot="bot-field"
   >
-  <div class="flex flex-wrap -mx-3 mb-6">
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+  <div class="flex flex-wrap -mx-3 mb-2">
+    <div class="w-full md:w-1/2 px-3 mb-2 md:mb-0">
     <input type="hidden" name="form-name" value="contact" />
       <label class="block  tracking-wide text-blue-900 text-xs  mb-2" for="name">
         Name
@@ -17,15 +17,15 @@ name="contact"
       <input class="appearance-none focus:outline-none block w-full bg-gray-200 text-blue-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white" id="name" name="name" required type="text">
     </div>
   </div>
-  <div class="flex flex-wrap -mx-3 mb-6">
-    <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+  <div class="flex flex-wrap -mx-3 mb-2">
+    <div class="w-full md:w-1/2 px-3 mb-2 md:mb-0">
       <label class="block  tracking-wide text-blue-900 text-xs  mb-2" for="email">
         E-mail
       </label>
       <input class="appearance-none focus:outline-none block w-full bg-gray-200 text-blue-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500" id="email" type="email" name="email" required>
     </div>
   </div>
-  <div class="flex flex-wrap -mx-3 mb-6">
+  <div class="flex flex-wrap -mx-3 mb-2">
     <div class="w-full px-3">
       <label class="block  tracking-wide text-blue-900 text-xs  mb-2" for="subject">
         Subject
@@ -33,7 +33,7 @@ name="contact"
       <input class="appearance-none focus:outline-none block w-full bg-gray-200 text-blue-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500" id="subject" type="subject" name="subject" required>
     </div>
   </div>
-  <div class="flex flex-wrap -mx-3 mb-6">
+  <div class="flex flex-wrap -mx-3 mb-2">
     <div class="w-full px-3">
       <label class="block  tracking-wide text-blue-900 text-xs  mb-2" for="message">
         Message
@@ -43,7 +43,7 @@ name="contact"
   </div>
   <div class="md:flex md:items-center">
     <div class="md:w-1/3">
-      <button class="shadow bg-blue-900 hover:bg-gray-400 focus:shadow-outline focus:outline-none text-white  py-2 px-4 rounded" type="submit" value="Send Message">
+      <button class="shadow mb-8 bg-blue-900 hover:bg-gray-400 focus:shadow-outline focus:outline-none text-white py-2 px-4 rounded" type="submit" value="Send Message">
         Send
       </button>
     </div>
--- pages/thanks.vue
+++ pages/thanks.vue
@@ -1,10 +1,10 @@
 <template>
       <div class="min-h-screen">
-
-      <div class="antialiased text-blue-900 text-4xl w-screen px-8 xl:w-2/3 mt-8 mx-auto mt-2">
-        Thank you! I will get back to you soon!
+      <div class="antialiased text-blue-900 text-2xl w-screen px-8 xl:w-2/3 mt-8 mx-auto mt-2">
+          <div class="w-screen px-8 xl:w-2/3 mx-auto mt-16 flex align-center justify-center h-screen">
+        Thank you! I will get back to you soon. :)
       </div>
-
+          </div>
     </div>
 </template>
 
