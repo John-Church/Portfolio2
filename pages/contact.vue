680e23e0950a4b1ebc891ef172d3e9ec0d2bae55
John <churchjohnr@gmail.com>
2020-11-13 15:21:17 -0800

    contact form style changes

--- pages/contact.vue
+++ pages/contact.vue
@@ -14,7 +14,7 @@ name="contact"
       <label class="block  tracking-wide text-blue-900 text-xs  mb-2" for="name">
         Name
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-blue-900 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:bg-white" id="name" name="name" required type="text" placeholder="Jane">
+      <input class="appearance-none focus:outline-none block w-full bg-gray-200 text-blue-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white" id="name" name="name" required type="text" placeholder="Jane">
     </div>
   </div>
   <div class="flex flex-wrap -mx-3 mb-6">
@@ -22,7 +22,7 @@ name="contact"
       <label class="block  tracking-wide text-blue-900 text-xs  mb-2" for="email">
         E-mail
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-blue-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500" id="email" type="email" name="email" required>
+      <input class="appearance-none focus:outline-none block w-full bg-gray-200 text-blue-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500" id="email" type="email" name="email" required>
     </div>
   </div>
   <div class="flex flex-wrap -mx-3 mb-6">
@@ -30,7 +30,7 @@ name="contact"
       <label class="block  tracking-wide text-blue-900 text-xs  mb-2" for="subject">
         Subject
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-blue-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500" id="subject" type="subject" name="subject" required>
+      <input class="appearance-none focus:outline-none block w-full bg-gray-200 text-blue-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500" id="subject" type="subject" name="subject" required>
     </div>
   </div>
   <div class="flex flex-wrap -mx-3 mb-6">
@@ -38,12 +38,12 @@ name="contact"
       <label class="block  tracking-wide text-blue-900 text-xs  mb-2" for="message">
         Message
       </label>
-      <textarea class=" no-resize appearance-none block w-full bg-gray-200 text-blue-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" name="message" required></textarea>
+      <textarea class=" no-resize appearance-none focus:outline-none block w-full bg-gray-200 text-blue-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:border-gray-500 h-48 resize-none" id="message" name="message" required></textarea>
     </div>
   </div>
   <div class="md:flex md:items-center">
     <div class="md:w-1/3">
-      <button class="shadow bg-blue-900 hover:bg-gray-400 focus:shadow-outline text-white  py-2 px-4 rounded" type="submit" value="Send Message">
+      <button class="shadow bg-blue-900 hover:bg-gray-400 focus:shadow-outline focus:outline-none text-white  py-2 px-4 rounded" type="submit" value="Send Message">
         Send
       </button>
     </div>
---
+++ pages/thanks.vue
@@ -0,0 +1,19 @@
+<template>
+      <div class="min-h-screen">
+
+      <div class="antialiased text-blue-900 text-4xl w-screen px-8 xl:w-2/3 mt-8 mx-auto mt-2">
+        Thank you! I will get back to you soon!
+      </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
